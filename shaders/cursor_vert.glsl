622bd0b7417726c015403edd5f5c64c5f2066fdc
Daniel Gu <endaytrer@gmail.com>
2023-10-19 23:39:45 +0800

    fix gles shader compilation error

--- shaders/cursor_vert.glsl
+++ shaders/cursor_vert.glsl
@@ -18,5 +18,5 @@ void main() {
         vert_position += vec2(0.0, size.y);
     }
 
-    gl_Position = vec4(2.0 * (vert_position.x - viewport_pos.x * viewport_scale) / (viewport_size.x * viewport_scale) - 1.0, 1.0 - 2.0 * (vert_position.y - viewport_pos.y * viewport_scale) / (viewport_size.y * viewport_scale) , 0.0, 1.0);
+    gl_Position = vec4(2.0 * (vert_position.x - viewport_pos.x * float(viewport_scale)) / (viewport_size.x * float(viewport_scale)) - 1.0, 1.0 - 2.0 * (vert_position.y - viewport_pos.y * float(viewport_scale)) / (viewport_size.y * float(viewport_scale)) , 0.0, 1.0);
 }
--- shaders/text_vert.glsl
+++ shaders/text_vert.glsl
@@ -23,6 +23,6 @@ void main() {
         vert_position += vec2(0.0, size.y);
         uv.y += uv_size.y;
     }
-    gl_Position = vec4(2.0 * (vert_position.x - viewport_pos.x * viewport_scale) / (viewport_size.x * viewport_scale) - 1.0, 1.0 - 2.0 * (vert_position.y - viewport_pos.y * viewport_scale) / (viewport_size.y * viewport_scale) , 0.0, 1.0);
+    gl_Position = vec4(2.0 * (vert_position.x - viewport_pos.x * float(viewport_scale)) / (viewport_size.x * float(viewport_scale)) - 1.0, 1.0 - 2.0 * (vert_position.y - viewport_pos.y * float(viewport_scale)) / (viewport_size.y * float(viewport_scale)) , 0.0, 1.0);
     frag_fg_color = fg_color;
 }
